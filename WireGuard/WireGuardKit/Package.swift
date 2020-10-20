2ee4a556ff819aed05701778d8c971b966f3bca5
Andrej Mihajlov <and@mullvad.net>
2020-10-20 19:59:34 +0200

    Swift WireGuardKit to dynamic lib

--- WireGuard/WireGuardKit/Package.swift
+++ WireGuard/WireGuardKit/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "WireGuardKit",
+            type: .dynamic,
             targets: ["WireGuardKit"]
         ),
         .library(
