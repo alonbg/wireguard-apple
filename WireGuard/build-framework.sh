3a0e49d57bb402f9bb7d9426d2c61027630f4448
Andrej Mihajlov <and@mullvad.net>
2020-10-21 17:41:54 +0200

    Add iOS simulator build

--- WireGuard/build-framework.sh
+++ WireGuard/build-framework.sh
@@ -9,8 +9,9 @@ XCODE_PROJECT_MACOS_SCHEME="WireGuardGoBridgemacOS"
 XCODE_PROJECT_IOS_SCHEME="WireGuardGoBridgeiOS"
 
 # Build folders
-MACOS_DERIVED_DATA_PATH="./WireGuardGoBridge.build/ios"
-IOS_DERIVED_DATA_PATH="./WireGuardGoBridge.build/macos"
+MACOS_DERIVED_DATA_PATH="./WireGuardGoBridge.build/macos"
+IOS_DERIVED_DATA_PATH="./WireGuardGoBridge.build/ios"
+IOSSIM_DERIVED_DATA_PATH="./WireGuardGoBridge.build/ios-sim"
 
 # Wireguard-go static library name
 LIB_NAME="libwg-go.a"
@@ -26,14 +27,23 @@ XCFRAMEWORK_DEPLOY_PATH="./WireGuardGoBridge/WireGuardGoBridge.xcframework"
 
 xcodebuild -project "$XCODE_PROJECT_PATH" \
   -configuration "$XCODE_PROJECT_CONFIGURATION" \
+  -sdk macosx \
   -scheme "$XCODE_PROJECT_MACOS_SCHEME" \
   -derivedDataPath "$MACOS_DERIVED_DATA_PATH" \
   clean build
 
 xcodebuild -project "$XCODE_PROJECT_PATH" \
   -configuration "$XCODE_PROJECT_CONFIGURATION" \
+  -sdk iphoneos \
   -scheme WireGuardGoBridgeiOS \
   -derivedDataPath "$IOS_DERIVED_DATA_PATH" \
+  clean build
+
+xcodebuild -project "$XCODE_PROJECT_PATH" \
+  -configuration "$XCODE_PROJECT_CONFIGURATION" \
+  -sdk iphonesimulator \
+  -scheme WireGuardGoBridgeiOS \
+  -derivedDataPath "$IOSSIM_DERIVED_DATA_PATH" \
   clean build
 
 if [ -d "$XCFRAMEWORK_OUTPUT_PATH" ]; then
@@ -46,6 +56,8 @@ xcodebuild -create-xcframework \
   -library "$MACOS_DERIVED_DATA_PATH/Build/Products/Release/$LIB_NAME" \
   -headers "$XCFRAMEWORK_HEADERS_PATH" \
   -library "$IOS_DERIVED_DATA_PATH/Build/Products/Release-iphoneos/$LIB_NAME" \
+  -headers "$XCFRAMEWORK_HEADERS_PATH" \
+  -library "$IOSSIM_DERIVED_DATA_PATH/Build/Products/Release-iphonesimulator/$LIB_NAME" \
   -headers "$XCFRAMEWORK_HEADERS_PATH" \
   -output "$XCFRAMEWORK_OUTPUT_PATH"
 
